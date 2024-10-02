13ad3accef0c2755b867913b3a1a4b41a3c248e2
GannaGavrylova <gannadeutsch@gmail.com>
2024-10-02 09:13:36 +0200

    19_ant_desing_form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Form, Input, Button, Card, Typography } from "antd";
+
+const { Title } = Typography;
 
 function App() {
+  const [formData, setFormData] = useState({ name: "", description: "" });
+  const [submittedData, setSubmittedData] = useState(null);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit() {
+    setSubmittedData(formData);
+    setFormData({ name: "", description: "" });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto" }}>
+      <Title>Форма с использованием Ant Design</Title>
+      <Form layout="vertical">
+        <Form.Item label="Имя">
+          <Input
+            name="name"
+            value={formData.name}
+            placeholder="Name"
+            onChange={handleChange}
+          />
+        </Form.Item>
+        <Form.Item label="Описание">
+          <Input
+            name="description"
+            value={formData.description}
+            placeholder="Description"
+            onChange={handleChange}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" onClick={handleSubmit}>
+            Отправить
+          </Button>
+        </Form.Item>
+      </Form>
+      {submittedData && (
+        <Card
+          title="Отправленные данные:"
+          bordered={false}
+          style={{
+            width: 300,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>Имя: {submittedData.name}</p>
+          <p>Описание: {submittedData.description}</p>
+        </Card>
+      )}
     </div>
   );
 }
